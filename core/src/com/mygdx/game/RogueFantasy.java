08f465476a0b95b81129365759c56fff01acea57
unknown <pedroigorpsampaio@gmail.com>
2024-04-30 20:05:29 +0100

    First commit: Initial version containing test login menu, localization, asset management and initial architecture

---
+++ core/src/com/mygdx/game/RogueFantasy.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class RogueFantasy extends Game {
+
+	public SpriteBatch batch;
+	public BitmapFont font;
+
+	public void create() {
+		batch = new SpriteBatch();
+		font = new BitmapFont(); // use libGDX's default Arial font
+		this.setScreen(new LoadScreen(this, "menu", new AssetManager()));
+	}
+
+	public void render() {
+		super.render(); // important!
+	}
+
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+
+}
